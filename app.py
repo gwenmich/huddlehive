bb8efd27f5d1ef80908683c8dcbfec441c9c0a89
Gwen Michailidis <79330147+gwenmich@users.noreply.github.com>
2026-06-27 16:49:49 +0100

    Change project name

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///streamtrue.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fancheck.db"
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 
 db.init_app(app)
