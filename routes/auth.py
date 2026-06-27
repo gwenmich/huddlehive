8390686121b1394021ac739b99ef670610735010
Gwen Michailidis <79330147+gwenmich@users.noreply.github.com>
2026-06-27 15:32:22 +0100

    Initial backend structure with spotifyAPI

---
+++ routes/auth.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+from models import User
+from extensions import db
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/auth/register", methods=["POST"])
+def register():
+    data = request.get_json()
+
+    if User.query.filter_by(email=data["email"]).first():
+        return jsonify({"error": "Email already registered"}), 400
+
+    hashed_password = generate_password_hash(data["password"])
+    user = User(email=data["email"], password = hashed_password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"message": "Account created!"}), 201
+
+
+@auth_bp.route("/auth/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(email=data["email"]).first()
+
+    if not user or not check_password_hash(user.password, data["password"]):
+        return jsonify({"error": "Invalid credentials"}), 401
+
+    token = create_access_token(identity=str(user.id))
+    return jsonify({"token": token}), 200
