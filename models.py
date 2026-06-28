df72a65606549c9daaa100a9746a32ecce8beaf0
Gwen Michailidis <79330147+gwenmich@users.noreply.github.com>
2026-06-28 08:10:47 +0100

    Fix password length

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from extensions import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(120), unique = True, nullable = False)
-    password = db.Column(db.String(20), nullable = False)
+    password = db.Column(db.String(200), nullable = False)
     spotify_access_token = db.Column(db.String(500), nullable = True)
     spotify_refresh_token = db.Column(db.String(500), nullable = True)
     spotify_token_expires_at = db.Column(db.DateTime, nullable = True)
